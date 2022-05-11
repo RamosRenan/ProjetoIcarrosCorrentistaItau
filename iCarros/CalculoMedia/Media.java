a87874829e7fa432b366b1e3324bf068275539f1
RamosRenan <renan.barbosards@gmail.com>
2022-05-10 23:04:14 -0300

    tarefa media icarros

---
+++ iCarros/CalculoMedia/Media.java
@@ -0,0 +1,62 @@
+//package CalculoMedia;
+
+import java.util.*;
+
+
+/**
+ * @author renan barbosa
+ *
+ * classe responsavel por scanear um nome de aluno e suas notas e realizar o calculo da média
+ */
+public class Media
+{
+	private int qtd_scores = 4;
+
+	private float notas;
+
+	private String nome;
+	
+	public Media(){}
+
+	public static void main(String[] args)
+	{
+		new Media().execScanner();
+	}
+
+	private void execScanner()
+	{
+		System.out.println("Iniciando pedido dos dados ! \n Insira seu nome: ");
+
+		Scanner sc = new Scanner(System.in); 
+
+		nome = sc.nextLine();
+		//sc.close();
+
+		int i = 0;
+		while(qtd_scores > 0)
+                {
+			System.out.println("Insira nota_"+(++i));
+		
+			try
+			{
+				notas +=sc.nextFloat();
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("Sua nota não é um valor válido reinsira os valores ...  "+e.getCause());
+				sc = new Scanner(System.in);
+				notas = 0.00f;
+				qtd_scores = 5;
+				i = 0;
+			}
+				
+		        --qtd_scores;
+                }
+
+		System.out.println("Sua somatória é: "+notas+"\n Sua média é: "+(notas / 4));
+
+	}
+
+	//gets and sets se necessário ...
+}
+
---
+++ iCarros/CalculoMedia/README.txt
@@ -0,0 +1,17 @@
+NomeProjeto = CalculoMedia
+classe=Media
+
+Nome_Aluno
+Nota1,Nota2,Nota3,Nota4,Media
+
+Calcular a média do aluno bom base nas notas
+
+Resultado
+==============================
+Mostrar o Nome do Aluno, as respectivas notas 
+adicionadas e a média final do aluno.
+
+OBS.: Todas as informações deverão ser imputadas
+através do Scanner.
+
+Enviar o mesmo para o github
