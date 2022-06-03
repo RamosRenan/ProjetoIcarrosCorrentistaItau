0ad773280d08f500b9cfcd187c54920aae109992
RamosRenan <renan.barbosards@gmail.com>
2022-06-03 00:50:59 -0300

    added comments in code

--- workspace/icarros_locadora/src/com/icarros/panels/PanelJFrame.java
+++ workspace/icarros_locadora/src/com/icarros/panels/PanelJFrame.java
@@ -1,16 +1,10 @@
 package com.icarros.panels;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
 
-import javax.swing.Action;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 
 import com.icarros.abstracts.BaseWindowApp;
 import com.icarros.components.AppJPanel;
@@ -18,26 +12,35 @@ import com.icarros.components.ButtonsAppUtils;
 import com.icarros.components.Formulario;
 import com.icarros.components.Tables;
 
-public class PanelJFrame extends BaseWindowApp{
-	
+public class PanelJFrame extends BaseWindowApp {
+
 	private Formulario formulario;
-	
+
 	private JFrame frame;
-	
+
 	private JScrollPane jTable;
-	
+
 	private ButtonsAppUtils buttonUtils;
-	
-	public PanelJFrame()
-	{
+
+	public PanelJFrame() {
 		formulario = new Formulario();
+
+		// crio instancia da classe tables para torna-la disponivel
 		new Tables();
+
+		// recupero a inatancia do formulario e envio para serer manipalado pelas ações
+		// e eventos dos botões
 		new ButtonsAppUtils(formulario);
 	}
-		
+
+	/**
+	 * @author renan barbosa
+	 * 
+	 *         crio componente JFrame como princpial da aplicação, componente pai de
+	 *         toda aplicação
+	 */
 	@Override
-	public JFrame createJFrameAppBase() 
-	{
+	public JFrame createJFrameAppBase() {
 		// TODO Auto-generated method stub
 		frame = new JFrame();
 		frame.setLayout(new BorderLayout());
@@ -50,44 +53,42 @@ public class PanelJFrame extends BaseWindowApp{
 	/**
 	 * @author renan barbosa
 	 * 
-	 * metodo responsavel por reunir componentes na tela principal
+	 *         metodo responsavel por reunir componentes na tela principal
 	 */
 	@Override
-	public JPanel createJPanelAppBase() 
-	{
+	public JPanel createJPanelAppBase() {
 		// TODO Auto-generated method stub
-		
+
 		AppJPanel.jPanel.add(this.formulario.getLabel());
-		
+
 		AppJPanel.jPanel.add(this.formulario.getLabelDuration());
 		AppJPanel.jPanel.add(this.formulario.getFieldDuration());
-		
+
 		AppJPanel.jPanel.add(this.formulario.getLabelGenero());
 		AppJPanel.jPanel.add(this.formulario.getFieldGenero());
-		
+
 		AppJPanel.jPanel.add(this.formulario.getLabelNome());
 		AppJPanel.jPanel.add(this.formulario.gettextFielNome());
-		
+
 		AppJPanel.jPanel.add(this.formulario.getLabelSinopse());
 		AppJPanel.jPanel.add(this.formulario.getFieldSinopse());
-		
+
 		AppJPanel.jPanel.add(this.formulario.getLabelData());
 		AppJPanel.jPanel.add(this.formulario.getFieldData());
-		
+
 		AppJPanel.jPanel.add(this.formulario.getFieldData());
-		
+
 		AppJPanel.jPanel.add(Tables.jScrollPane);
-		
+
 		AppJPanel.jPanel.add(ButtonsAppUtils.btnSair);
 		AppJPanel.jPanel.add(ButtonsAppUtils.btnCreate);
-		
+
 		this.frame.add(AppJPanel.jPanel);
-		
+
 		return AppJPanel.jPanel;
 	}
-	
-	public JPanel getJpanelApp() 
-	{
+
+	public JPanel getJpanelApp() {
 		return AppJPanel.jPanel;
 	}
 }
